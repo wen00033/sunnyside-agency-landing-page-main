64a8e4d1dc95735d99cc8df91105c9318453e06a
Chi-hung Wen <wen00033@algonquinlive.com>
2023-05-25 11:17:10 -0400

    updated

---
+++ sunnyside-agency-landing-page-main/script.js
@@ -0,0 +1,36 @@
+// const numbers = [1, 2, 3, 4];
+
+// console.log(numbers.filter((a) => a > 1));
+
+// const newarr = numbers.map((a) => a * 25 + 10);
+// console.log(newarr);
+
+// const students = [
+//   { name: "T-shirt", Lastname: "a-shirt", price: 1000 },
+//   { name: "chicken", Lastname: "-bshirt", price: 2 },
+//   { name: "A-shirt", Lastname: "v-shirt", price: 5 },
+//   { name: "shirt", Lastname: "f-shirt", price: 85 },
+//   { name: "pant", Lastname: "f-shirt", price: 45 },
+// ];
+
+// const newaa = students.sort((a, b) =>
+//   a.Lastname.toLowerCase() > b.Lastname.toLowerCase()
+//     ? 1
+//     : a.Lastname.toLocaleLowerCase() < b.Lastname.toLowerCase()
+//     ? -1
+//     : 0
+// );
+// console.log(newaa);
+
+// const productToal = product.reduce((a, b) => a + b.price, 0);
+// console.log(productToal);
+
+// const productarr = product.filter((a) => a.price > 50);
+// console.log(productarr);
+// const findarr = product.find((a) => a.price > 0);
+// console.log(findarr);
+
+// const names = ["Alice", "Bob", "Charlie", "David", "Eva"];
+
+// const news = names.find((a) => a.startsWith("D")).toUpperCase();
+// console.log(news);
